f9320808c624f61b85a326d404f063ea53789fc6
Calvin <43611393+ccrino@users.noreply.github.com>
2021-11-23 18:31:41 -0600

    Initial commit

---
+++ windows/cypherscanner/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::cypherscanner::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::cypherscanner::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
